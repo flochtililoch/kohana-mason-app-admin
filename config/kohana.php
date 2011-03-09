9114f99b50ab7402789e6ff32698e0facf95e752
florent <florent@0e2ab60e-4180-4ff9-b8f3-7acb517cb61e>
2011-03-09 18:37:05 +0000

    follow up to changes in configuration for caching, profiling and logging (now set by env)
    git-svn-id: http://192.168.0.50:8000/svn/apps/admin/trunk@16 0e2ab60e-4180-4ff9-b8f3-7acb517cb61e

--- config/kohana.php
+++ config/kohana.php
@@ -5,11 +5,31 @@ return array
 	'timezone'		=> 'Europe/Paris',			// Server Timezone
 	'i18n_source'	=> 'en-gb',					// Default language used in templates
 	'debug'			=> Kohana::DEBUG_JSON,		// Set level of debug (FALSE = disabled, Kohana::DEBUG_JSON, Kohana::DEBUG_HTML)
-	'caching'		=> FALSE,					// Enable or disable internal caching
+	'caching'		=> array(					// Caching status per env.
+		Kohana::DEVELOPMENT => FALSE,
+		Kohana::TESTING => TRUE,
+		Kohana::STAGING => FALSE,
+		Kohana::PRODUCTION => TRUE
+		),
 		'cache_life'	=> 0,
 		'cache_dir'		=> APPPATH.'var/cache',
-	'profile'		=> FALSE,					// Enable or disable internal profiling
-	'logging'		=> TRUE,					// Enable or disable internal logging	
+	'profile'		=> array(					// Profiling status per env.
+		Kohana::DEVELOPMENT => TRUE,
+		Kohana::TESTING => FALSE,
+		Kohana::STAGING => FALSE,
+		Kohana::PRODUCTION => FALSE
+		),
+	'logging'		=> array(					// Logging status per env.
+		Kohana::DEVELOPMENT => TRUE,
+		Kohana::TESTING => TRUE,
+		Kohana::STAGING => TRUE,
+		Kohana::PRODUCTION => FALSE
+		),
 		'log_dir'		=> APPPATH.'var/log',
-	'errors'		=> TRUE
+	'errors'		=> array(					// Display errors per env.
+		Kohana::DEVELOPMENT => TRUE,
+		Kohana::TESTING => TRUE,
+		Kohana::STAGING => TRUE,
+		Kohana::PRODUCTION => FALSE
+		)
 );
