911cd91d82b2d0958f968ec1ccb6c26654cf1665
florent <florent@0e2ab60e-4180-4ff9-b8f3-7acb517cb61e>
2011-03-09 18:41:51 +0000

    app creation comp - wip
    git-svn-id: http://192.168.0.50:8000/svn/apps/admin/trunk@17 0e2ab60e-4180-4ff9-b8f3-7acb517cb61e

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	'cl' => array(
+		'cvs_export' => 'svn export https://florent:kfa4nkz1@canal69.hd.free.fr:8443/svn/apps/default/trunk %1$s',
+		'i18n' => 'mkdir %1$s/i18n/%2$s/LC_MESSAGES'
+	)
+);
