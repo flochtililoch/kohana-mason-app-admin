e49429701bb82dea1ec520e70caa5ec95306d3f4
florent <florent@0e2ab60e-4180-4ff9-b8f3-7acb517cb61e>
2011-04-20 19:16:24 +0000

    added : app download step
    git-svn-id: http://192.168.0.50:8000/svn/apps/admin/trunk@23 0e2ab60e-4180-4ff9-b8f3-7acb517cb61e

--- config/app.php
+++ config/app.php
@@ -3,7 +3,12 @@
 return array
 (
 	'cl' => array(
-		'cvs_export' => 'svn export https://florent:kfa4nkz1@canal69.hd.free.fr:8443/svn/apps/default/trunk %1$s',
-		'i18n' => 'mkdir %1$s/i18n/%2$s/LC_MESSAGES'
+		'cvs_export' => 'svn export http://192.168.0.50:8000/public/skeleton/trunk %1$s 2>&1',
+		'i18n' => array(
+			'container' => 'mkdir %1$s/i18n/%2$s',
+			'lc_messages' => 'mkdir %1$s/i18n/%2$s/LC_MESSAGES'
+		),
+		'pack' => 'cd %1$s && tar -czf ../%2$s.zip .  && rm ../%3$s -rf'
+		
 	)
 );
